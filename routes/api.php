b128a9963538f4a807d8fc71bd8a20a5085d0850
kameih <jaikameih@gmail.com>
2024-02-07 15:34:48 +0530

    uploading all file for Laravel API

---
+++ routes/api.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ApiController;
+use App\Http\Controllers\ProductController;
+
+Route::post('login', [ApiController::class, 'authenticate']);
+Route::post('register', [ApiController::class, 'register']);
+
+
+// It will allow this page to run only if token is confirmed and valid
+Route::group(['middleware' => ['jwt.verify']], function() {
+    Route::get('logout', [ApiController::class, 'logout']);
+    Route::get('get_user', [ApiController::class, 'get_user']);
+});
